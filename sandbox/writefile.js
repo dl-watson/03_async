5df4882079d57f0270d687c64ee135f82be5283c
dl-watson <dlarkinwatson@gmail.com>
2020-12-02 13:49:22 -0800

    calling writeFile() or writeFileAsync() works as expected

---
+++ sandbox/writefile.js
@@ -0,0 +1,28 @@
+const fs = require("fs").promises;
+
+const writeFile = () => {
+  return fs
+    .writeFile(
+      "./writefile_then.txt",
+      "fs.writeFile using then chaining",
+      "utf-8",
+      (err, data) => {
+        if (err) throw err;
+        console.log("The file has been saved.");
+      }
+    )
+    .then((data) => console.log(data));
+};
+
+const writeFileAsync = async () => {
+  try {
+    const file = await fs.writeFile(
+      "./writefile_async.txt",
+      "fs.writeFile using async/await",
+      "utf-8"
+    );
+    return file;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+};
