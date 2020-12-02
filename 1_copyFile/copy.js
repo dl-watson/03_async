2c70fc5504e34488ecb9de48142b5669d16b018f
dl-watson <dlarkinwatson@gmail.com>
2020-12-02 15:41:06 -0800

    copy() works as intended, tests passing

--- 1_copyFile/copy.js
+++ 1_copyFile/copy.js
@@ -2,12 +2,15 @@ const fs = require("fs").promises;
 
 const copy = (src, dst) => {
   return fs
-    .readFile(src, "utf-8", (err, data) => {
+    .readFile(src, "utf-8", (err) => {
       if (err) throw err;
     })
-    .then((data) => console.log(data));
+    .then((data) => {
+      data += `\nAnd writeFile works, too!`;
+      fs.writeFile(dst, data, "utf-8", (err) => {
+        if (err) throw err;
+      });
+    });
 };
 
 module.exports = { copy };
-
-copy("./README.md");
--- 1_copyFile/copy.test.js
+++ 1_copyFile/copy.test.js
@@ -3,12 +3,12 @@ const { copy } = require("./copy");
 
 describe("copy", () => {
   afterEach(() => {
-    return fs.rm('./src.txt', './dst.txt');
+    return fs.rm("./dst.txt");
   });
 
-  it("reads then writes a file", () => {
-    await copy('./src.txt', './dst.txt');
-    const contents = await fs.readFile('./dst.txt', 'utf-8');
-    expect(contents).toEqual(`if you're reading this, copy() works as intended`);
+  it("reads then writes a file", async () => {
+    await copy("./1_copyFile/src.txt", "./dst.txt");
+    const contents = await fs.readFile("./dst.txt", "utf-8");
+    expect(contents).toEqual(`readFile works!\nAnd writeFile works, too!`);
   });
 });
