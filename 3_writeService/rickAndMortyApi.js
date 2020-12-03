733d6246b207974f3a772484b88073a93625d790
dl-watson <dlarkinwatson@gmail.com>
2020-12-02 16:47:52 -0800

    setting up tests

---
+++ 3_writeService/rickAndMortyApi.js
@@ -0,0 +1,3 @@
+const rickAndMortyApi = () => {};
+
+module.exports = { rickAndMortyApi };
---
+++ 3_writeService/rickAndMortyApi.test.js
@@ -0,0 +1,6 @@
+const { rickAndMortyApi } = require("./rickAndMortyApi");
+const fs = require("fs").promises;
+
+describe("rickAndMortyApi", () => {
+  it("takes an id and returns a promise that resolves to a character's name, status and species", () => {});
+});
