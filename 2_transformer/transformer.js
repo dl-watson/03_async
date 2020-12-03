0aee8661347d897aaa3979332156fee7fd94e15f
dl-watson <dlarkinwatson@gmail.com>
2020-12-02 16:36:35 -0800

    transformer() working as expected

--- 2_transformer/transformer.js
+++ 2_transformer/transformer.js
@@ -1,7 +1,31 @@
-const transformer = (src) => {};
+const fsPromises = require("fs").promises;
+const _ = require("lodash");
+const src = "./palindrome.txt";
+
+const transformer = async (src) => {
+  try {
+    const req = await fsPromises.readFile(src, "utf-8");
+    const lowercase = req.replace(/[A-Z]*/g, (letter) => letter.toLowerCase());
+    const uppercase = lowercase
+      .replace(/[a-z]*/g, (letter) => letter.toUpperCase())
+      .split("");
+    const reversed = _.reverse(uppercase).join("");
+
+    const munged = await fsPromises.writeFile(
+      "./emordnilap.txt",
+      reversed,
+      "utf-8",
+      (err) => {
+        if (err) throw err;
+      }
+    );
+
+    return munged;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
+};
 
 module.exports = { transformer };
 
-/* 
-str.replace( /[^a-z]/g, '' );
-*/
+// transformer(src);
--- 2_transformer/transformer.test.js
+++ 2_transformer/transformer.test.js
@@ -1,6 +1,7 @@
 const fs = require("fs").promises;
 const { transformer } = require("./transformer");
-const src = "./2_transformer/palindrome.txt";
+const input_src = "./2_transformer/palindrome.txt";
+const output_src = "./2_transformer/emordnilap.txt";
 const string = `Dammit I’m mad.
 Evil is a deed as I live.
 God, am I reviled? I rise, my bed on a sun, I melt.
@@ -30,11 +31,40 @@ Name not one bottle minus an ode by me:
 “Sir, I deliver. I’m a dog”
 Evil is a deed as I live.
 Dammit I’m mad.`;
+const munged = `.DAM M’I TIMMAD
+.EVIL I SA DEED A SI LIVE
+”GOD A M’I .REVILED I ,RIS“
+:EM YB EDO NA SUNIM ELTTOB ENO TON EMAN
+.EMAN A TI NGISSA D’I .)PIS( SALA SI TI SSOL A
+!TEW HO .STOPS TOH ELPMA NI TIS ,HO
+.TI M’I .NI TI TEL LIAM NO
+.TA DEVIL I LEVEL MADDOG A
+,HSA YBUR NI NGIS A SA DEIFIED ,GULP I NMYH A
+.LOOF A M’I ?REDRUM
+.TI NI ERA SEMAN YM …ENO ,HTOB
+.EMAN A SI NIS ?EVIL I NAC ,LAMINA TRAP
+.NOITISOP ENO NI TI WAS STUC KRATS ,PEED
+!ON HO .PEELS EW
+?HE …REDIPS A ,WE
+.BBE STI LLIF I FI LLITS EB
+.BEW A SPID EREHWESLE .EPOH NO DEEP STAR
+.KCUTS SAW I TI NO .NEPO SI TI .ON
+?PART A NI ?MALC A NI LIVE SI
+.NIS A ,NAM EB OT ,HO .NEM YNAM ESARE I
+.NIT MUR DERIM A FO OLAH YM NI
+,PLUG DEIFIED A SA ,SI GNINRUB YAS ,HA
+.”GOD DAM“ LEVEL I .LIVED A TON MA I
+.LLET I .TI NI M’I .TOH SI TI ,NAM
+?PLEH OT SPOTS OHW .ETAL OS SI TI ,SALA
+.SSIP I .DAS SI GNITANAME NAM ENO TON EB OT
+.TLEM I ,NUS A NO DEB YM ,ESIR I ?DELIVER I MA ,DOG
+.EVIL I SA DEED A SI LIVE
+.DAM M’I TIMMAD`;
 
 describe("transformer", () => {
-  it("reads a file, removes all capitals, makes all letters capital, then reverses the string", async () => {
-    await transformer(src);
-    const contents = await fs.readFile(src, "utf-8");
-    expect(contents).toEqual(string);
+  it("reads a file, removes all capitals with lowercase, makes all letters capital, then reverses the string", async () => {
+    await transformer(input_src);
+    const contents = await fs.readFile(output_src, "utf-8");
+    expect(contents).toEqual(munged);
   });
 });
