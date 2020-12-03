c556c78baac6e0278ba2e07375564273b5a2ecb0
dl-watson <dlarkinwatson@gmail.com>
2020-12-02 18:24:04 -0800

    nested in a try/catch block

--- 3_writeService/getManyCharacters_async.js
+++ 3_writeService/getManyCharacters_async.js
@@ -1,14 +1,18 @@
 const fetch = require("node-fetch");
 
 const getManyCharacters = async (ids) => {
-  const urls = urlFormatter(ids);
-  const promises = await Promise.all(urls.map((url) => fetch(url)));
-  const json = await Promise.all(promises.map((res) => res.json()));
-  const results = json.map(
-    (data) =>
-      `Name: ${data.name}, Status: ${data.status}, Species: ${data.species}`
-  );
-  return results;
+  try {
+    const urls = urlFormatter(ids);
+    const promises = await Promise.all(urls.map((url) => fetch(url)));
+    const json = await Promise.all(promises.map((res) => res.json()));
+    const results = json.map(
+      (data) =>
+        `Name: ${data.name}, Status: ${data.status}, Species: ${data.species}`
+    );
+    return results;
+  } catch (err) {
+    console.log("Error: ", err);
+  }
 };
 
 const urlFormatter = (ids) => {
