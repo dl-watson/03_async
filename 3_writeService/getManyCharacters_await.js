a3ce9c817477a405a6b9aca857ef7ed0c5a4f591
dl-watson <dlarkinwatson@gmail.com>
2020-12-02 18:17:51 -0800

    setting up tests for async/await refactor

---
+++ 3_writeService/getManyCharacters_await.js
@@ -0,0 +1,26 @@
+const fetch = require("node-fetch");
+
+const getManyCharacters_async = async (ids) => {
+  const urls = urlFormatter(ids);
+  const promises = await Promise.all(
+    urls.map((url) =>
+      fetch(url).then((res) =>
+        res
+          .json()
+          .then(
+            (data) =>
+              `Name: ${data.name}, Status: ${data.status}, Species: ${data.species}`
+          )
+      )
+    )
+  );
+  return promises;
+};
+
+const urlFormatter = (ids) => {
+  const url = `https://rickandmortyapi.com/api/character/`;
+
+  return ids.map((id) => `${url}${id}`);
+};
+
+module.exports = { getManyCharacters_async };
